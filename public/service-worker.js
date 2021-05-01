e75298095c51e41da9a3763b0f72896fd2e12006
Karen <karenyaa@live.co.uk>
2021-05-01 17:00:54 +0100

    added eventlistener to install cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,12 @@ const FILES_TO_CACHE = [
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
+
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
