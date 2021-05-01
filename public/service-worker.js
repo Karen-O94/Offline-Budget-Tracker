892fc23e0ff7e2c5ba3f48b5ed5657cc57fd1ed4
Karen <karenyaa@live.co.uk>
2021-05-01 17:03:03 +0100

    refracted function to install cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,16 +5,17 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/manifest.webmanifest",
     "/db.js"
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+];
 
-  self.addEventListener('install', (event) => {
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-  });
+    self.skipWaiting();
+});
